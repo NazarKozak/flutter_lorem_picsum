3df1b02f04a6842d6e041bc2c3ba4540409912aa
Nazar Kozak <nzrkzk@gmail.com>
2021-12-20 00:56:00 +0200

    Update project structure

---
+++ lib/app/application.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_lorem_picsum/screens/screens.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Lorem Picsum',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/config/api_constants.dart
@@ -0,0 +1,3 @@
+class ApiConstants {
+  static const String baseUrl = "https://picsum.photos/v2/";
+}
